cde7524a92584b58336ea4f4353426ef138a72fc
Tomek Marciniak <tmarciniakm@gmail.com>
2024-01-08 22:03:54 +0100

    chore(app): move to node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [vitePreprocess({})],
-
 	kit: {
 		adapter: adapter(),
 		alias: {
