f23f5748f5f8d18ab1320981803e3489e9c8680d
Tomek Marciniak <tmarciniakm@gmail.com>
2024-01-09 23:50:41 +0100

    chore(app): move to libsql

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
 	schema: './src/lib/db/schema.ts',
 	out: './drizzle',
-	driver: 'libsql',
+	driver: (process.env.SECRET_DB_DRIVER ?? '') as 'turso',
 	dbCredentials: {
-		url: 'file:sqlite.db'
+		url: process.env.SECRET_SQLITE_URL ?? '',
+		authToken: process.env.SECRET_SQLITE_KEY ?? ''
 	},
 	verbose: true,
 	strict: true
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from '$lib/db/schema';
+import { SECRET_SQLITE_URL, SECRET_SQLITE_KEY } from '$env/static/private';
 
-export const client = createClient({ url: 'file:sqlite.db' });
+export const client = createClient({ url: SECRET_SQLITE_URL, authToken: SECRET_SQLITE_KEY });
 export const db = drizzle(client, { schema });
