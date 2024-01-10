4e416e90aa266dd0e87e13cc35986a64e229ba64
Tomek Marciniak <tmarciniakm@gmail.com>
2024-01-10 07:55:28 +0100

    chore(app): add catch to migrations

--- migrate.ts
+++ migrate.ts
@@ -4,7 +4,11 @@ import { migrate } from 'drizzle-orm/libsql/migrator';
 import { db } from './src/lib/db';
 
 const main = async () => {
-	await migrate(db, { migrationsFolder: 'drizzle' });
+	try {
+		await migrate(db, { migrationsFolder: 'drizzle' });
+	} catch (e) {
+		console.log('>>>MIGRATION_FAILED', e);
+	}
 	process.exit(0);
 };
 
