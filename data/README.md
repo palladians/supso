3e51d673a51170b53da4f0215c80cb68ce2d6c95
Tomek Marciniak <tmarciniakm@gmail.com>
2024-02-11 23:01:28 +0100

    chore(app): recreate migrations and move db to /data

---
+++ data/README.md
@@ -0,0 +1,3 @@
+# Data directory
+
+This directory is mounted for 1-click deployments in Railway.
--- drizzle/0000_round_cyclops.sql
+++ drizzle/0000_round_cyclops.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `access_token` (
 	`id` text PRIMARY KEY NOT NULL,
+	`internal` numeric DEFAULT 'true',
 	`name` text NOT NULL,
 	`user_id` text NOT NULL,
 	`created_at` text,
@@ -13,9 +14,10 @@ CREATE TABLE `board` (
 	`name` text NOT NULL,
 	`tag` text NOT NULL,
 	`options` text NOT NULL,
+	`events_order` text,
 	`created_at` text,
 	`updated_at` text,
-	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `event` (
@@ -28,9 +30,13 @@ CREATE TABLE `event` (
 	`notify` numeric DEFAULT 'false',
 	`tags` text,
 	`context` text,
+	`priority` text DEFAULT 'normal' NOT NULL,
+	`assignee_id` text,
+	`due_date` text,
 	`created_at` text,
 	`updated_at` text,
-	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`assignee_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `feature_flag` (
@@ -41,7 +47,7 @@ CREATE TABLE `feature_flag` (
 	`enabled` numeric DEFAULT 'false',
 	`created_at` text,
 	`updated_at` text,
-	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `project` (
@@ -51,6 +57,14 @@ CREATE TABLE `project` (
 	`created_at` text,
 	`updated_at` text,
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `project_invitation` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`project_id` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `user` (
@@ -81,7 +95,7 @@ CREATE TABLE `users_to_projects` (
 	`project_id` text NOT NULL,
 	`role` text DEFAULT 'member' NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `verification_code` (
@@ -91,5 +105,16 @@ CREATE TABLE `verification_code` (
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE TABLE `webhook` (
+	`id` text PRIMARY KEY NOT NULL,
+	`project_id` text NOT NULL,
+	`url` text NOT NULL,
+	`service` text NOT NULL,
+	`created_at` text,
+	`updated_at` text,
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `project_invitation_user_id_project_id_unique` ON `project_invitation` (`user_id`,`project_id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);--> statement-breakpoint
 CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);
